7c626dea5e861aded5c953ed68141725b2722ff6
DpoofikD <konarevms@gmail.com>
2022-03-05 21:49:39 +0500

    yeeeeah

--- app/src/main/java/com/example/principiacustodes/MainActivity.java
+++ app/src/main/java/com/example/principiacustodes/MainActivity.java
@@ -16,5 +16,6 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // some changes
         // new branch btw
+        // best commit ever
     }
 }
